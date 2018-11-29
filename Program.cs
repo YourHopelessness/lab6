cc818ac6905095ca12c6e57f2cff1809806e7a70
NesposobniyLev <45355061+NesposobniyLev@users.noreply.github.com>
2018-11-29 17:18:34 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,10 +10,29 @@ namespace ConsoleApp2
     {
        static void Main(string[] argv)
         {
+            bool flag = false;
+            int fMult, sMult;
+            fMult = sMult = 0;
             Console.WriteLine("Введите множители ");
-            string[] input = Console.ReadLine().Split(' ');
 
-            Console.WriteLine("Произведение = {0}", Multiple(int.Parse(input[0]), int.Parse(input[1])));
+            while (!flag)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                
+                flag = true;
+                try
+                {
+                    fMult = int.Parse(input[0]);
+                    sMult = int.Parse(input[1]);
+                }
+                catch
+                {
+                    flag = false;
+                    Console.WriteLine("Неккоректный ввод");
+                }
+            }
+
+            Console.WriteLine("Произведение = {0}", Multiple(fMult, sMult));
         }
         static long Multiple(int first, int second)
         {
