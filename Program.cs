558bb95ef86a9965020fb0fab091d56ee6848374
YourHopelessness <45352437+YourHopelessness@users.noreply.github.com>
2018-11-29 16:12:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+       static void Main(string[] argv)
+        {
+            Console.WriteLine("Введите множители ");
+            string[] input = Console.ReadLine().Split(' ');
+
+            Console.WriteLine("Произведение = {0}", Multiple(int.Parse(input[0]), int.Parse(input[1])));
+        }
+        static long Multiple(int first, int second)
+        {
+            long mult = first;
+            for (int i = 1; i < second; i++)
+                mult += first;
+            return mult;
+        }
+    }
+}
